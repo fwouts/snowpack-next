7e497046aea009b172a04bd0b479cf183a510cac
François Wouts <f@zenc.io>
2021-04-12 17:54:37 +1000

    Set up Snowpack

---
+++ index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./pages/_app";
+import Home from "./pages";
+
+ReactDOM.render(<App Component={Home} />, document.getElementById("root"));
